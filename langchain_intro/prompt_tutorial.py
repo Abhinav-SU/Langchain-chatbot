99fb9143210d4ffe480a8dd342f79f4d9f527096
Your Name <your.email@example.com>
2025-10-02 17:45:50 -0700

    Prompt Template tutorial and chatbot creation

--- langchain_intro/prompt_tutorial.py
+++ langchain_intro/prompt_tutorial.py
@@ -1,5 +1,8 @@
+#Importing ChatPromptTemplate from langchain.prompts 
 from langchain.prompts import ChatPromptTemplate
 
+
+#Basic review template string with context and question
 review_template_str = """Your job is to use patient reviews to answer 
 question about their expeience at a hospital.
 Use the following context to answer questions. Be as detailed
@@ -12,10 +15,12 @@ If you dont know an answer, say you dont know.
 {question}
 """
 
+#Creating a review template from review template string using the ChatPromptTemplate
 review_template = ChatPromptTemplate.from_template(review_template_str)
 
-
+#Providing the context and the question from user
 context= "I had a great stay!"
 question ="Did anyone have a positive experience?"
 
+#Printing the result of the review tempate after formatting it
 print(review_template.format(context=context,question=question))
